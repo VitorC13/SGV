0af859cb8b808a928b64465555d717b5956ac476
Vitor Cardoso <vitorcardoso.desenvolvedor@gmail.com>
2017-03-05 19:17:38 -0300

    AcessoController
    Autentica o paciente

---
+++ src/main/java/com/grupotcc/sgv/controller/AcessoController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupotcc.sgv.controller;
+
+import com.grupotcc.sgv.dao.PacientesDAO;
+import com.grupotcc.sgv.model.Paciente;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author vitor
+ */
+public class AcessoController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String acao = request.getParameter("acao");
+            if (acao.equals("Entrar")) {
+                Paciente paciente = new Paciente();
+                paciente.setNome(request.getParameter("txtLogin"));
+                paciente.setSenha(request.getParameter("txtSenha"));
+                PacientesDAO usuarioDAO = new PacientesDAO();
+                Paciente usuarioAutenticado = usuarioDAO.autenticaPaciente(paciente);
+                //se o usuario existe no banco de dados
+                if (usuarioAutenticado != null) {
+                    //cria uma sessao para o usuario
+                    HttpSession sessaoUsuario = request.getSession();
+                    sessaoUsuario.setAttribute("usuarioAutenticado", usuarioAutenticado);
+                    //redireciona para a pagina princiapal
+                    response.sendRedirect("principal.jsp");
+                } else {
+                    RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+                    request.setAttribute("msg", "Login ou Senha Incorreto!");
+                    rd.forward(request, response);
+                }
+            } else if (acao.equals("Sair")) {
+                HttpSession sessaoUsuario = request.getSession();
+                sessaoUsuario.removeAttribute("usuarioAutenticado");
+                response.sendRedirect("logout.jsp");
+            }
+        } catch (Exception erro) {
+            RequestDispatcher rd = request.getRequestDispatcher("/erro.jsp");
+            request.setAttribute("erro", erro);
+            rd.forward(request, response);
+        }
+    }
+}
